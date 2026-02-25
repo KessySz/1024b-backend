9ed1069d879c71ae86e29f4fb4c3ef9078839c97
KessySz <kesia.souza@estudante.ifms.edu.br>
2026-02-25 10:20:19 -0400

    Consts

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,49 @@ let vetor:number[] = [1,2,3,4,5]
 
 let nomes:string[] = ["Hii~","Hello!"]
 console.log(nomes)
+
+//Teste
+
+// let bol:boolean = true
+
+// bol = false
+
+// const variavel:any = "oi"
+
+// const a:undefined = undefined;
+
+// const obj:{id:number;nome:string;idade:number} ={
+//     id:1, 
+//     nome:"Kesia",
+//     idade:18
+// }
+
+// obj.nome = "Souza"
+
+const x1 = 10
+
+const vetor1: 
+number[] = [0,1,2,3,4]
+vetor1[0] = 10
+
+// vetor1 = [0,1,2,3,4]
+
+let vetor2 = [0,1,2,3,4]
+vetor2 = [0,1,2,3,4]
+
+let aob: number|string = 10 ;
+aob= "julia"
+
+const obj:{id?:number;nome:string;idade:number} ={
+    
+    nome:"Kesia",
+    idade:18
+}
+
+type Pessoa = {id?:number;nome:string;idade:number} 
+
+const p:Pessoa={
+    id:76,
+    nome:"Fulana",
+    idade:31
+}
