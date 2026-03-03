5ba1e7b77f575975709e8aec84b61f44b965a1a7
KessySz <kesia.souza@estudante.ifms.edu.br>
2026-03-03 11:42:42 -0400

    funcoes

--- src/index.ts
+++ src/index.ts
@@ -52,3 +52,29 @@ const p:Pessoa={
     nome:"Fulana",
     idade:31
 }
+
+// Funções em Javascript/typescript
+
+//o que é função?
+/**
+ * um bloco de código que executa uma tarefa
+ * (tarefa é uma sequência de comandos de código)
+ * 
+ * Benefício: Não repetir código
+ */
+
+const f =  (variavel:number):number=>10
+const f3 = f(10)
+console.log("O valor de x é "+f3.toString())
+console.log(`O valor de x é ${f3}`)
+
+function SomaVetor(vetor:number[]) {
+  let soma = 0
+  for (let i = 0; i < vetor.length; i++)   {
+    const element = vetor[i];
+    soma = soma + element!
+  }
+ return soma
+}
+console.log(`O valor da soma do vetor é ${SomaVetor([1,2,3])}`)
+
